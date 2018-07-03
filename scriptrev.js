c543adc765578e0484adbe503b5f2cfd6e568289
tmaitland <tmaitland@techlaunch.io>
2018-07-02 21:24:16 -0400

    first commit

---
+++ scriptrev.js
@@ -0,0 +1,47 @@
+// Create a variable and set its value to the answer of a math equation. It should be a number Value
+
+var ans = 5 * 6
+console.log(ans)
+
+
+// Create a variable and set its value to 5 strings being concatenated together using the ‘+’ operator. Each string should be at least 10 characters long. Try and make a real paragraph that a human will be able to read and understand.
+
+var para = {
+    name: "My name is Toni-lee Maitland.",
+    age: "I am 26 years old.",
+    education: "I graduated from the University of Florida.",
+    occupation: "I taught Social Studies to middle and high schoolers for 3 years.",
+    goal: "I want to become a web developer after years of thinking about it."
+}
+
+console.log(para.name +" "+ para.age +" "+ para.education +" "+ para.occupation +" "+ para.goal)
+
+//Create a variable and use the ES6 string interpolation (back ticks) to set the value of the variable to a string that includes 5 variables whose variables are numbers. Use ${} to use these number variables in the string variable.
+
+var info = {
+    
+}
+
+//Create an array  that has 4 numbers, 2 strings, 3 comparisons, another array with 3 elements inside.
+ 
+var arr = [5, 6, 7, 8,"help", "me", 8==9, 5>3, 6!=7]
+var wow = {
+    mood: frustrated,
+    test: anxiety,
+    brain: blank,
+    speed: slow
+}
+
+//Create an object that has 4 properties: A number, a string, an array, and an object
+
+var popQuiz = {
+    month: 7,
+    birthMon:  "November",
+    monthsLeft = ["August", "September","October","November", "December"],
+    daysOfTheWeek = {
+        first: "Sunday",
+        second: "Monday",
+        third: "Tuesday"
+    }
+}
+
